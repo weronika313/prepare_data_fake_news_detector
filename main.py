b6a941483ecd989ff163eb9f80b459bf66911c50
Weronika Żochowiec <Veronica.zochowiec@gmail.com>
2021-03-30 18:15:57 +0200

    refactor

--- main.py
+++ main.py
@@ -1,51 +1,47 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from wordcloud import WordCloud, STOPWORDS
-import nltk
-import re
 from nltk.corpus import stopwords
-import seaborn as sns
 import gensim
-from gensim.utils import simple_preprocess
-from gensim.parsing.preprocessing import STOPWORDS
-import pickle
-
-import plotly.express as px
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
-from sklearn.metrics import confusion_matrix
 import joblib
 
 
 def preprocess(text):
     result = []
     for token in gensim.utils.simple_preprocess(text):
-        if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 2 and token not in stop_words:
+        if (
+            token not in gensim.parsing.preprocessing.STOPWORDS
+            and len(token) > 2
+            and token not in stop_words
+        ):
             result.append(token)
 
     return result
 
+
 df_fake = pd.read_csv("input/Fake.csv")
 df_true = pd.read_csv("input/True.csv")
 
-df_true['target'] = 1
-df_fake['target'] = 0
+df_true["target"] = 1
+df_fake["target"] = 0
 df = pd.concat([df_true, df_fake]).reset_index(drop=True)
-df['original'] = df['title'] + ' ' + df['text']
+df["original"] = df["title"] + " " + df["text"]
 
-stop_words = stopwords.words('english')
-stop_words.extend(['from', 'subject', 're', 'edu', 'use'])
+stop_words = stopwords.words("english")
+stop_words.extend(["from", "subject", "re", "edu", "use"])
 
-df.subject=df.subject.replace({'politics':'PoliticsNews','politicsNews':'PoliticsNews'})
+df.subject = df.subject.replace(
+    {"politics": "PoliticsNews", "politicsNews": "PoliticsNews"}
+)
 
-df['clean_final'] = df['original'].apply(preprocess)
-df['clean_joined_final']=df['clean_final'].apply(lambda x:" ".join(x))
+df["clean_final"] = df["original"].apply(preprocess)
+df["clean_joined_final"] = df["clean_final"].apply(lambda x: " ".join(x))
 
-X_train, X_test, y_train, y_test = train_test_split(df.clean_joined_final, df.target, test_size = 0.25,random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(
+    df.clean_joined_final, df.target, test_size=0.25, random_state=0
+)
 vec_train = CountVectorizer().fit(X_train)
 X_vec_train = vec_train.transform(X_train)
 X_vec_test = vec_train.transform(X_test)
@@ -54,8 +50,5 @@ model.fit(X_vec_train, y_train)
 predicted_value = model.predict(X_vec_test)
 accuracy_value = roc_auc_score(y_test, predicted_value)
 
-filename = 'fake_news_model.pkl'
+filename = "fake_news_model.pkl"
 joblib.dump(model, filename)
-
-
-
