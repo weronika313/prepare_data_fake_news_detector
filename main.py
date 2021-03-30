ee065d86608f92040d929a1b4231b5e87aa8d1f0
Weronika Żochowiec <Veronica.zochowiec@gmail.com>
2021-03-30 17:08:21 +0200

    clean news title

--- main.py
+++ main.py
@@ -18,6 +18,15 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import confusion_matrix
 
+
+def preprocess(text):
+    result = []
+    for token in gensim.utils.simple_preprocess(text):
+        if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 2 and token not in stop_words:
+            result.append(token)
+
+    return result
+
 df_fake = pd.read_csv("input/Fake.csv")
 df_true = pd.read_csv("input/True.csv")
 
@@ -25,7 +34,15 @@ df_true['target'] = 1
 df_fake['target'] = 0
 df = pd.concat([df_true, df_fake]).reset_index(drop=True)
 df['original'] = df['title'] + ' ' + df['text']
-print(df.head())
+
+stop_words = stopwords.words('english')
+stop_words.extend(['from', 'subject', 're', 'edu', 'use'])
+
+df.subject=df.subject.replace({'politics':'PoliticsNews','politicsNews':'PoliticsNews'})
+
+df['clean_title'] = df['title'].apply(preprocess)
+df['clean_joined_title']=df['clean_title'].apply(lambda x:" ".join(x))
+
 
 
 
