8e7582703ce0639d3db31f8fd04612a581abd7ae
Weronika Żochowiec <Veronica.zochowiec@gmail.com>
2021-03-30 16:47:12 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from wordcloud import WordCloud, STOPWORDS
+import nltk
+import re
+from nltk.corpus import stopwords
+import seaborn as sns
+import gensim
+from gensim.utils import simple_preprocess
+from gensim.parsing.preprocessing import STOPWORDS
+
+import plotly.express as px
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import confusion_matrix
+
+df_fake = pd.read_csv("input/Fake.csv")
+df_true = pd.read_csv("input/True.csv")
+
+
+
+
+
